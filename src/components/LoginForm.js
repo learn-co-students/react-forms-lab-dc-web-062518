cf987d7261848ed4ae9fcc81ede67288c655eba1
github username <github email address>
2018-08-30 18:35:19 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,23 +4,38 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
+
+
+  handleSubmit = e=>{
+    e.preventDefault()
+    if(this.state.username !== '' && this.state.password !== ''){
+      this.props.onSubmit({username: this.state.username, password: this.state.password})
+    }
+  }
+
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={ e=> this.setState({username: e.target.value})} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
-          </label>
+            <input id="password" name="password" type="password" value={this.state.password} onChange={ e=> this.setState({password: e.target.value})}/>
+
+          // <input type="text" value={this.state.value} onChange={e => this.setState({value: e.target.value})}/>
+
+        </label>
         </div>
         <div>
           <button type="submit">Log in</button>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -3,15 +3,18 @@ import React from "react";
 class TwitterMessage extends React.Component {
   constructor() {
     super();
-
-    this.state = {};
+    this.state = {
+      value: ''
+    };
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+
+        <input type="text" value={this.state.value} onChange={e => this.setState({value: e.target.value})}/>
+        <p>Remaining characters = {this.props.maxChars - this.state.value.length}</p>
       </div>
     );
   }
